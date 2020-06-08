2e3e1c6db3ef46d9416de96e41ba8538a3dafcce
Dylan Edwards <dylanedwards290@gmail.com>
2020-06-07 23:54:00 -0700

    Search and pull search pages

--- Economist_searcher/Economist_searcher/spiders/webcrawler.py
+++ Economist_searcher/Economist_searcher/spiders/webcrawler.py
@@ -26,8 +26,12 @@ class EconomistSpider(scrapy.Spider):
     start_urls = urls
 
     def parse(self, response):
-        page = response.url.split('=')[-1]
+        substring = '&page'
+        #page = item
+        page = response.url.split('=')[1]
         for result in response.css('ol.layout-search-results li'):
+            if substring in page:
+                page = page.replace('&page', '')
             yield {
                 'Link': result.css('li a.search-result::attr(href)').get(),
                 'title': result.css('span.search-result__headline::text').get(),
@@ -37,6 +41,29 @@ class EconomistSpider(scrapy.Spider):
         next_page = 'https://www.economist.com/search' + response.css('li.ds-pagination__nav--next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+        entries = []
+        duplicate_entries = []
+        with open('output.csv', 'r') as my_file:
+            for line in my_file:
+                columns = line.strip().split(',')
+                if columns[1] not in entries:
+                    entries.append(columns[1])
+                else:
+                    duplicate_entries.append(columns[1])
+
+        if len(duplicate_entries) > 0:
+            with open('duplicates.csv', 'w') as out_file:
+                with open('output.csv', 'r') as my_file:
+                    for line in my_file:
+                        columns = line.strip().split(',')
+                        if columns[1] in duplicate_entries:
+                            print line.strip()
+                            out_file.write(line)
+        else:
+            with open('none.txt', 'w') as file:file.write("none")
+
+
 
 
 # response.css('a.search-result::attr(href)').getall()
