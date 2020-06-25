c62111e450d2064e01c09c7d7332de0c9a7f71cd
Justin Li <justinnhli@gmail.com>
2020-06-25 01:40:13 -0700

    fix everything!
    * Factor out filenames as constants
    * Use `Spider.start_urls()` instead of `start_urls` class variable
    * Change output format from csv to json
    * Simplify de-duplication and ranking into a single function

--- webcrawler.py
+++ webcrawler.py
@@ -1,122 +1,86 @@
+import json
+from collections import defaultdict
+from csv import DictWriter
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
-import os
+from scrapy.http.request import Request
 
+KEYWORDS_FILE = 'textfile.txt'
+SCRAPY_OUTPUT_FILE = 'scrapy-output.json'
+RANKED_OUTPUT_FILE = 'output.csv'
 
-urls = []
-search_matrix = []
-items = []
-new_items = []
-entries = []
-duplicate_entries = []
-unique_duplicates = []
-
-def GetLinks():
-    path = os.getcwd()
-    path = path + "/textfile.txt"
-    my_file = open(path)  # opens textfile for keyword retrival
-
-    all_the_lines = my_file.readlines()
-
-
-    for i in all_the_lines:
-        items.append(i)
-
-    new_items = [x[:-1] for x in items]
-
-
-    for item in new_items:
-        urls.append('https://www.economist.com/search?q=' + item);  # creates URLs by adding keywords to search link
-
-
-    for i in range(len(new_items)):
-        search_matrix.append([])  # creates a search matrix for use later in ordering csv output by term
-
-
-#end of 1st function
 
 class EconomistSpider(scrapy.Spider):
+
     name = "economist"
-    start_urls = urls
+
+    def start_requests(self):
+        # see https://stackoverflow.com/a/10379463
+        for keyword in read_keywords():
+            yield Request('https://www.economist.com/search?q=' + keyword.strip(), self.parse)
 
     def parse(self, response):
-        substring = '&page'
-        # page = item
-        page = response.url.split('=')[1]
+        word = response.url.split('=')[1]
+        if '&page' in word:
+            word = word.replace('&page', '')
+        # yield info requested
         for result in response.css('ol.layout-search-results li'):
-            if substring in page:
-                page = page.replace('&page', '')
             yield {
                 'Link': result.css('li a.search-result::attr(href)').get(),
-                'title': result.css('span.search-result__headline::text').get(),  # yields for info requested
-                'word': page
+                'title': result.css('span.search-result__headline::text').get(),
+                'word': word,
             }
-
-        next_page = 'https://www.economist.com/search' + response.css('li.ds-pagination__nav--next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)  # follows to the next page of that search term
-
-
-
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'csv',
-    'FEED_URI': 'output.csv',
-})
-
- # the script will block here until the crawling is finished
-
-def Duplicates():
-    with open('output.csv', 'r') as my_file:
-        for line in my_file:
-            columns = line.strip().split(',')
-            if columns[1] not in entries:
-                entries.append(columns[1])  # populates entries and duplicates arrays
-            else:
-                duplicate_entries.append(columns[1])
-
-    if len(duplicate_entries) > 0:
-        with open('duplicates.csv', 'w') as out_file:
-            with open('output.csv', 'r') as my_file:
-                for line in my_file:
-                    columns = line.strip().split(',')
-                    if (columns[1] in duplicate_entries) and (columns[1] not in unique_duplicates):  # adds duplicate items to file, Output items are put into file by pipleine
-                        unique_duplicates.append(
-                            columns[1])  # ^ stops same one from going in but ommits same link diff search
-                        print line.strip()
-                        out_file.write(line)
+        # follow to the next page of that search term
+        next_page_params = response.css('li.ds-pagination__nav--next a::attr(href)').get()
+        if next_page_params is not None:
+            next_page = 'https://www.economist.com/search' + next_page_params
+            yield response.follow(next_page, callback=self.parse)
 
 
+def read_keywords():
+    with open(KEYWORDS_FILE) as fd:
+        return [keyword.strip() for keyword in fd.readlines()]
 
-# No longer works after refactoring of code
-def IndividualSearches():
-    for item in new_items:
-        with open('output.csv', 'r') as my_file:  # puts search terms into nested array list by term name
-            for line in my_file:
-                columns = line.strip().split(',')
-                if columns[0] == item:
-                    position = new_items.index(str(
-                        item))  # the index of the searh item which cooresponds to the search matrix index for each term
-                    search_matrix[position].append(columns[0])
 
-    for items in new_items:
-        if len(search_matrix) > 0:
-            name = items + ".csv"
-            with open("test", 'w') as out_file:
-                with open('output.csv', 'r') as my_file:
-                    for line in my_file:
-                        columns = line.strip().split(',')
-                        if columns[0] in search_matrix[new_items.index(items)]:
-                            print line.strip()
-                            out_file.write(line)
+def collate():
+    # read in results from crawler
+    result_keywords = defaultdict(set)
+    result_titles = {}
+    with open(SCRAPY_OUTPUT_FILE) as fd:
+        results = json.load(fd)
+    for result in results:
+        url = result['Link']
+        result_keywords[url].add(result['word'])
+        result_titles[url] = result['title']
+    # sort results by the number of keywords matched
+    ranked_urls = sorted(result_keywords, key=(lambda url: len(result_keywords[url])), reverse=True)
+    # write results
+    keywords = read_keywords()
+    with open(RANKED_OUTPUT_FILE, 'w') as fd:
+        writer = DictWriter(fd, fieldnames=['url', 'title', *keywords])
+        writer.writeheader()
+        for url in ranked_urls:
+            writer.writerow({
+                'url': url,
+                'title': result_titles[url],
+                'keywords': str(', '.join(sorted(result_keywords[url]))),
+            })
 
+
+def main():
+    process = CrawlerProcess(settings={
+        'FEEDS': {
+            SCRAPY_OUTPUT_FILE: {
+                'format': 'json',
+            },
+        },
+    })
+    process.crawl(EconomistSpider)
+    process.start()
+    # the script will block here until the crawling is finished
+    collate()
 
 
 if __name__ == "__main__":
-    GetLinks()
-    process.crawl(EconomistSpider)
-    process.start()
-    Duplicates()
-    IndividualSearches()
-
-
+    main()
